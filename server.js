697dbbb1ad25d0bee8ee729168399ea3dcb7faf2
Jonathan <nonathan.lim@gmail.com>
2026-09-19 09:39:28 +0800

    added new folder for better organisation and added SGDS package

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve the frontend files
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 
 // =====================================================
